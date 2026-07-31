f7f63cbc82cc767e371e27e06fb23c952e0d1100
Tomas Larsson <larre.b.larsson@gamil.comn>
2026-07-31 10:15:56 +0200

    Bump new release due to changelog error

--- src/scarpa_connection_manager.py
+++ src/scarpa_connection_manager.py
@@ -6492,7 +6492,7 @@ if logger.f: logger.f.close()
             transient_for=parent_window, # Can be None
             modal=True,
             program_name=APP_TITLE,
-            version="1.2.23",
+            version="1.2.24",
             authors=["Gemini, Tomas Larsson"],
             artists=["Tomas Larsson"],
             comments="A GTK-based SSH/SFTP session manager.\nRiposa in pace, Aquila di Filottrano. Sarai sempre con noi!"
